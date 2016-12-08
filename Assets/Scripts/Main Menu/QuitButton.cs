b3f81bb2d7871645c71714046fca1b9b26314fc7
clpachec <carlleninpacheco@gmail.com>
2016-12-07 16:50:52 -0800

    Adding Jared Art

--- Assets/Scripts/Main Menu/QuitButton.cs
+++ Assets/Scripts/Main Menu/QuitButton.cs
@@ -6,7 +6,7 @@ public class QuitButton : MonoBehaviour {
 	// Use this for initialization
 	public void Quit()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+        //UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 }
